3a41153ef520974346bc2fc5fad1ec8ca2d4c02b
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-02 18:55:16 -0500

    author insert, update, deleteand get api

--- src/main/java/com/realdb/finalproject/author/AuthorController.java
+++ src/main/java/com/realdb/finalproject/author/AuthorController.java
@@ -2,11 +2,10 @@ package com.realdb.finalproject.author;
 
 import com.realdb.finalproject.entity.Author;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author jeremy on 2022/12/2
@@ -18,8 +17,37 @@ public class AuthorController {
 
     private final AuthorService authorService;
 
-    @GetMapping()
+    @GetMapping("/all")
     public List<Author> getAuthors() {
         return authorService.getAuthors();
+    }
+
+    @GetMapping()
+    public Optional<Author> getAuthor(@RequestParam Integer id) {
+        return authorService.getAuthor(id);
+    }
+
+    @PostMapping()
+    public void saveAuthor(@RequestBody Author author) {
+        authorService.saveAuthor(author);
+    }
+
+    // /api/author?id=1
+    @DeleteMapping()
+    public void deleteAuthor(@RequestParam Integer id) {
+        authorService.deleteAuthor(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateAuthor(
+            @PathVariable("id") Integer id,
+            @RequestParam(required = false) String country,
+            @RequestParam(required = false) Integer zipcode,
+            @RequestParam(required = false) String fName,
+            @RequestParam(required = false) String lName,
+            @RequestParam(required = false) String street,
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String email) {
+        authorService.updateAuthor(id, country, zipcode, fName, lName, street, city, email);
     }
 }
--- src/main/java/com/realdb/finalproject/author/AuthorService.java
+++ src/main/java/com/realdb/finalproject/author/AuthorService.java
@@ -3,8 +3,11 @@ package com.realdb.finalproject.author;
 import com.realdb.finalproject.entity.Author;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author jeremy on 2022/12/2
@@ -17,5 +20,75 @@ public class AuthorService {
 
     public List<Author> getAuthors() {
         return authorRepo.findAll();
+    }
+
+    public Optional<Author> getAuthor(Integer id) {
+        return authorRepo.findById(id);
+    }
+
+    public void saveAuthor(Author author) {
+        authorRepo.save(author);
+    }
+
+    public void deleteAuthor(Integer id) {
+        boolean exists = authorRepo.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException(
+                    "Author with id " + id + " does not exists");
+        }
+        authorRepo.deleteById(id);
+    }
+
+    @Transactional
+    public void updateAuthor(Integer id,
+                             String country,
+                             Integer zipcode,
+                             String fName,
+                             String lName,
+                             String street,
+                             String city,
+                             String email) {
+        Author author = authorRepo.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Author with id " + id + " does not exist"
+                ));
+
+        if (fName != null && fName.length() > 0
+                && !Objects.equals(author.getAFname(), fName)) {
+            author.setAFname(fName);
+        }
+
+        if (lName != null && lName.length() > 0
+                && !Objects.equals(author.getALname(), lName)) {
+            author.setALname(lName);
+        }
+
+        if (country != null && country.length() > 0
+                && !Objects.equals(author.getACountry(), country)) {
+            author.setACountry(country);
+        }
+
+        if (street != null && street.length() > 0
+                && !Objects.equals(author.getAStreet(), street)) {
+            author.setAStreet(street);
+        }
+
+        if (city != null && city.length() > 0
+                && !Objects.equals(author.getACity(), city)) {
+            author.setACity(city);
+        }
+
+        if (zipcode != null && !Objects.equals(author.getAZipcode(), zipcode)) {
+            author.setAZipcode(zipcode);
+        }
+
+        if (email != null && email.length() > 0
+                && !Objects.equals(author.getAEmail(), email)) {
+            Optional<Author> studentOptional = authorRepo.findAuthorByAemail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            author.setAEmail(email);
+        }
     }
 }
