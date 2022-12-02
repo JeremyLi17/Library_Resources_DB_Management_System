134580e640b91b5bd02418fcf1bd744b30545901
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-02 18:54:00 -0500

    update spring security

---
+++ src/main/java/com/realdb/finalproject/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.realdb.finalproject.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+/**
+ * @author jeremy on 2022/12/2
+ */
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().antMatchers("/").permitAll();
+        return http.build();
+    }
+}
