b5b0a1dad117b8329563bb391296bcc79309c117
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-02 19:59:11 -0500

    fix bugs

--- src/main/java/com/realdb/finalproject/author/AuthorRepo.java
+++ src/main/java/com/realdb/finalproject/author/AuthorRepo.java
@@ -2,12 +2,14 @@ package com.realdb.finalproject.author;
 
 import com.realdb.finalproject.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * @author jeremy on 2022/12/2
  */
+@Repository
 public interface AuthorRepo extends JpaRepository<Author, Integer> {
     Optional<Author> findAuthorByAemail(String email);
 }
