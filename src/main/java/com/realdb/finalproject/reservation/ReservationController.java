a45cddad7e68129fc64dfdbd738d1a35d937ee0b
mac <mac@macdeMacBook-Pro-4.local>
2022-12-10 11:06:28 -0500

    reservation-api

---
+++ src/main/java/com/realdb/finalproject/reservation/ReservationController.java
@@ -0,0 +1,43 @@
+package com.realdb.finalproject.reservation;
+import com.realdb.finalproject.reservation.ReservationService;
+import com.realdb.finalproject.entity.Author;
+import com.realdb.finalproject.entity.Customer;
+import com.realdb.finalproject.entity.StudyRoom;
+import com.realdb.finalproject.entity.Reservation;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/reservation")
+@AllArgsConstructor
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @PostMapping()
+    public void addReservation (@RequestBody Reservation reservation){
+        reservationService.addReservation(reservation);
+    }
+    @GetMapping()
+    public Optional<Reservation> getReservation(@RequestParam Integer id) {
+        return reservationService.getReservation(id);
+    }
+    @GetMapping(path = "{userid}")
+    public List<Reservation> findAllResByUserID(@RequestParam Integer userid){
+        return reservationService.findAllResByUserID(userid);
+    }
+    @PutMapping(path = "{reservationid}")
+    public void updateReservation(
+            @PathVariable("id") Integer id,
+            @RequestParam(required = false) LocalDate resDate,
+            @RequestParam(required = false) String resTimeslot,
+            @RequestParam(required = false) StudyRoom studyRoomRoom
+           ) {
+
+        reservationService.updateReservation(id, resDate, resTimeslot, studyRoomRoom);
+    }
+
+}
