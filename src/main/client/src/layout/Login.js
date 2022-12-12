ff77dcebbe47c307217a93553daa67a8c160115b
MichaelXiaoKun <kxiao855@gmail.com>
2022-12-12 18:20:46 -0500

    added authentication in login page

--- src/main/client/src/layout/Login.js
+++ src/main/client/src/layout/Login.js
@@ -4,6 +4,10 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import '../layout_attributes/Login.css';
 import Employee from './Employee'
 import {useState} from 'react';
+import userRequest from '../request/user-request';
+import axios from 'axios';
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("token")}`
 
 export default function Login() {
 
@@ -11,12 +15,94 @@ export default function Login() {
     const [username, setUsername] = useState('');
     const [pwd, setPwd] = useState('');
 
-    const handleSubmit = event => {
+    const customerLogInRequest = async (e) => {
+        e.preventDefault();
+
+        setUsername(e.target.value);
+        setPwd(e.target.value);
+        
+        const data = {
+            username,
+            password: pwd
+        }
+        
+        const res = await axios.post(
+            "http://localhost:8080/api/customer/login",
+            data
+        )
+
+        if (res.status === 200) {
+            localStorage.setItem("token", res.headers["jwt-token"]);
+            // TODO: Add customer navigation
+        }
+
+    };
+
+    // const onclick = async (e) => {
+    //     e.preventDefault();
+        
+    //     const data = {
+    //         username: '1210test',
+    //         password: 'password'
+    //     }
+
+    //     const res = await axios.post(
+    //         "http://localhost:8080/api/customer/login",
+    //         data
+    //     )
+
+
+    //     console.log(res);
+    //     localStorage.setItem("token", res.headers["jwt-token"]);
+    //     // console.log(localStorage.getItem("token"));
+
+    //     // const config = {
+    //     //     headers: {
+    //     //         "Authorization": `Bearer ${localStorage.getItem("token")}`
+    //     //     }
+    //     // };
+
+    //     const res2 = await axios.get(
+    //         "http://localhost:8080/api/customer/list",
+    //     )
+
+    //     console.log(res2);
+    // }
+
+    const employeeLogInRequest = async (e) => {
         // 👇️ prevent page refresh
-        event.preventDefault();
-        console.log('username is: ', username);
-        console.log('pwd is: ', pwd);
-        console.log('form submitted ✅');
+        e.preventDefault();
+
+        setUsername(e.target.value);
+        setPwd(e.target.value);
+        
+        console.log(username);
+        console.log(pwd);
+        const data = {
+            username,
+            password: pwd
+        }
+        
+        const res = await axios.post(
+            "http://localhost:8080/api/employee/login",
+            data
+        ).then((res)=> {
+            console.log('success');
+            localStorage.setItem("token", res.headers["jwt-token"]);
+            localStorage.setItem("currentUser", res.data);
+            navigateToEmployeeDashboard();
+        }).catch((e) => {
+            console.log(e.response.data['message']);
+            sessionStorage.setItem('err', res.response.data['message'])
+            navigateToPwd_error();
+        })
+
+        
+        // if (res.status === 200) {
+        //     localStorage.setItem("token", res.headers["jwt-token"]);
+        //     localStorage.setItem("currentUser", res.data);
+        //     navigateToEmployeeDashboard();
+        // }
     };
 
     const handleChangeUsername = event => {
@@ -44,7 +130,7 @@ export default function Login() {
                     Sign in to Real Management System
                 </h1>
             </header>
-            <form className='Input' onSubmit={handleSubmit}>
+            <form className='Input'>
                 <div className="InputBox">
                     <div>
                         <label> Username </label>
@@ -71,8 +157,8 @@ export default function Login() {
                     onChange={handleChangePwd}/>
 
                     <div>
-                        <button onClick={navigateToPwd_error}>Customer Sign in</button>
-                        <button onClick={navigateToEmployeeDashboard}>Employee Sign in</button>
+                        <button onClick={onclick}>Customer Sign in</button>
+                        <button onClick={employeeLogInRequest}>Employee Sign in</button>
                     </div>
 
                     <div>
@@ -99,7 +185,7 @@ function Employee_DashBoard() {
 }
 
 function Pwd_error() {
-    return <h2 className="wrong_pwd">wrong username or password</h2>;
+    return <h2 className="wrong_pwd">{sessionStorage.getItem('err')}</h2>;
 }
 
 function Home() {
