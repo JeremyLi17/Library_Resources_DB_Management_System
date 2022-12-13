dd63058967705aeecb29c3dc7bcbb15c4ce21c66
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-13 07:08:27 -0500

    email service

---
+++ src/main/java/com/realdb/finalproject/utility/EmailConstant.java
@@ -0,0 +1,20 @@
+package com.realdb.finalproject.utility;
+
+/**
+ * @author jeremy on 2022/12/9
+ */
+public class EmailConstant {
+    public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
+    public static final String USERNAME = "email";
+    public static final String PASSWORD = "password";
+    public static final String FROM_EMAIL = "support@real.com";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_SUBJECT = "REAL DB - Your Password has been reset";
+    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+}
