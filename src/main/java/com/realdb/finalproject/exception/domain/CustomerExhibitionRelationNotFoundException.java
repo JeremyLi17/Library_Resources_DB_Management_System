8969e9b2e39863f721633fcea1830e4b6e93a4be
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-13 05:37:23 -0500

    adjust customerExhibition delete api

---
+++ src/main/java/com/realdb/finalproject/exception/domain/CustomerExhibitionRelationNotFoundException.java
@@ -0,0 +1,11 @@
+package com.realdb.finalproject.exception.domain;
+
+/**
+ * @author jeremy on 2022/12/13
+ */
+public class CustomerExhibitionRelationNotFoundException
+        extends Exception{
+    public CustomerExhibitionRelationNotFoundException(String message) {
+        super(message);
+    }
+}
