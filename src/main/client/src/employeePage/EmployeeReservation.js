62e0b884a53263492e148653c854042133e2f4eb
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-13 00:57:26 -0500

    fix default header bug

--- src/main/client/src/employeePage/EmployeeReservation.js
+++ src/main/client/src/employeePage/EmployeeReservation.js
@@ -1,61 +1,52 @@
 import '../employeePageAttribute/EmployeeReservation.css';
-import React, { Component } from 'react'
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import {useState} from 'react';
+import React, { Component } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import axios from 'axios';
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("token")}`
-
-const reservationList = ({list}) => {
-    return (
-        <div className='Home_Reservation'>
-            <h1>Reservation Info:</h1>
-            <ul>
-                {list.map((el) => {
-                    return (
-                        <li key={list.id}>
-                            <hr></hr>
-                            <div className='Home_Reservation_List'>
-                                <div className='Home_Reservation_Info'>
-                                    <div className='Home_Reservation_Info_Line'>
-                                    <div className='Home_Reservation_Info_Block'>
-                                        <div className='Home_Reservation_Info_Text'>Room Capacity: {}</div>
-                                    </div>
-                                    <div className='Home_Reservation_Info_Block'>
-                                        <div className='Home_Reservation_Info_Text'>Room Capacity: {}</div>
-                                    </div>
-                                    <div className='Home_Reservation_Info_Block'>
-
-                                    </div>
-                                    </div>
-                                    <div className='Home_Reservation_Info_Line'>
-                                    <div className='Home_Reservation_Info_Block'>
-
-                                    </div>
-                                    <div className='Home_Reservation_Info_Block'>
-
-                                    </div>
-                                    <div className='Home_Reservation_Info_Block'>
-
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-            <hr></hr>
-        </div>
-    );
-}
+const reservationList = ({ list }) => {
+  return (
+    <div className="Home_Reservation">
+      <h1>Reservation Info:</h1>
+      <ul>
+        {list.map((el) => {
+          return (
+            <li key={list.id}>
+              <hr></hr>
+              <div className="Home_Reservation_List">
+                <div className="Home_Reservation_Info">
+                  <div className="Home_Reservation_Info_Line">
+                    <div className="Home_Reservation_Info_Block">
+                      <div className="Home_Reservation_Info_Text">
+                        Room Capacity: {}
+                      </div>
+                    </div>
+                    <div className="Home_Reservation_Info_Block">
+                      <div className="Home_Reservation_Info_Text">
+                        Room Capacity: {}
+                      </div>
+                    </div>
+                    <div className="Home_Reservation_Info_Block"></div>
+                  </div>
+                  <div className="Home_Reservation_Info_Line">
+                    <div className="Home_Reservation_Info_Block"></div>
+                    <div className="Home_Reservation_Info_Block"></div>
+                    <div className="Home_Reservation_Info_Block"></div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <hr></hr>
+    </div>
+  );
+};
 
 export default async function EmployeeReservation() {
-
-    // localStorage.getItem('')
-    const [date, setDate] = useState();
-    const [timeslot, setTimeslot] = useState();
-    const [studyRoom, setStudyRoom] = useState();
-
-
+  // localStorage.getItem('')
+  const [date, setDate] = useState();
+  const [timeslot, setTimeslot] = useState();
+  const [studyRoom, setStudyRoom] = useState();
 }
