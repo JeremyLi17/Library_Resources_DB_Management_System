293807bfa56f79f08cdaa2f6af0dcb4dbca3ce61
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-13 09:26:32 -0500

    fix invoice api bug

--- src/main/java/com/realdb/finalproject/entity/payment/PaymentController.java
+++ src/main/java/com/realdb/finalproject/entity/payment/PaymentController.java
@@ -1,6 +1,7 @@
 package com.realdb.finalproject.entity.payment;
 
 import com.realdb.finalproject.domain.HttpResponse;
+import com.realdb.finalproject.exception.domain.InvoiceNotFoundException;
 import com.realdb.finalproject.exception.domain.PaymentNotFoundException;
 import com.realdb.finalproject.utility.BuildResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +30,9 @@ public class PaymentController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Payment> addPayment(@RequestBody Payment payment) {
-        paymentService.makePayment(payment.getPaymentAmount(), payment.getMethod(), payment.getCardHolderFullName());
+    public ResponseEntity<Payment> addPayment(@RequestBody Payment payment) throws InvoiceNotFoundException {
+        paymentService.makePayment(payment.getPaymentAmount(), payment.getMethod(),
+                payment.getCardHolderFullName(), payment.getInvoice().getId());
         return new ResponseEntity<>(payment, CREATED);
     }
 
